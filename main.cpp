31cf251ffead3d2fddd7f80bd893daebd12f8d2c
PodySIN <daniil2007xboctob@gmail.com>
2025-12-08 17:22:28 +0300

    Get image in terminal (3 Dots)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ namespace top {
   struct IDraw {
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
+    virtual ~IDraw() = default;
   };
 
   bool operator==(p_t a, p_t b)
@@ -27,47 +28,65 @@ namespace top {
     Dot(int x, int y);
   };
 
-  struct Frame_t {
-    p_t left_bot;
-    p_t right_bot;
+  struct f_t {
+    p_t aa;
+    p_t bb;
   };
-  
+  size_t rows(f_t fr);
+  size_t cols(f_t fr);
+  void extend(p_t** pts, size_t s, p_t p);
   void make_f(IDraw ** b, size_t k);
-  void get_points(IDraw * b, p_t ** ps, size_t & s);
-  Frame_t build_frame(const p_t * ps, size_t s);
-  char * build_canvas(Frame_t f);
-  void paint_canvas(char * cnv, Frame_t fr, const p_t * ps, size_t k, char f);
-  void print_canvas(const char * cnv, Frame_t fr);
+  size_t points(const IDraw& d, p_t ** pts, size_t  s);
+  f_t frame(const p_t * ps, size_t s);
+  char * canvas(f_t f, char fill);
+  void paint(char * cnv, f_t fr, p_t p, char fill);
+  void flush(std::ostream& os, const char* cnv, f_t fr);
 }
 
 int main()
 {
-  using namespace top;
-  IDraw * f[3] = {};
-  p_t * p = nullptr;
-  char * cnv = nullptr;
+  using top::IDraw;
+  using top::Dot;
+  using top::f_t;
+  using top::p_t;
+  int err = 1;
+  IDraw* shps[3] = {};
+  p_t * pts = nullptr;
   size_t s = 0;
-  int err = 0;
   try {
-    make_f(f, 3);
+    shps[0] = new Dot(0, 0);
+    shps[1] = new Dot(5, 7);
+    shps[2] = new Dot(-5, -2);
     for (size_t i = 0; i < 3; i++) {
-      get_points(f[i], &p, s);
+      s+= points(*(shps[i]), &pts, s);
     }
-    Frame_t fr = build_frame(p, s);
-    cnv = build_canvas(fr);
-    paint_canvas(cnv, fr, p, s, '#');
-    print_canvas(cnv, fr);
-  } catch (...) {
-    err = 1;
-    delete[] f[0];
-    delete[] f[1];
-    delete[] f[2];
-    delete[] p;
+    f_t fr = frame(pts, s);
+    char * cnv = canvas(fr, '.');
+    for (size_t i = 0; i < s; i++) {
+      paint(cnv, fr, pts[i], '#');
+    }
+    flush(std::cout, cnv, fr);
     delete[] cnv;
-    return err;
+    
+  } catch (...) {
+    err = 2;
+    std::cerr << "Dab drawing\n";
   }
+  delete[] pts;
+  delete shps[0];
+  delete shps[1];
+  delete shps[2];
+  return err;
 }
 
+size_t top::rows(f_t fr)
+{
+  return (fr.bb.y - fr.aa.y + 1);
+}
+size_t top::cols(f_t fr)
+{
+  return (fr.bb.x - fr.aa.x + 1);
+}
 top::Dot::Dot(int x, int y):
   IDraw(),
   o{x, y}
@@ -81,4 +100,73 @@ top::p_t top::Dot::begin() const
 top::p_t top::Dot::next(p_t) const
 {
   return begin();
+}
+
+void top::extend(p_t** pts, size_t s, p_t p) 
+{
+
+  p_t *res = new p_t[s+1];
+  for(size_t i = 0; i < s; i++) {
+    res[i] = (*pts)[i];
+  }
+  res[s] = p;
+  delete[] *pts;
+  *pts = res;
+}
+
+size_t top::points(const IDraw& d, p_t ** pts, size_t s)
+{
+  p_t p = d.begin();
+  extend(pts, s, p);
+  size_t delta = 1;
+  while (d.next(p) != d.begin()) {
+    p = d.next(p);
+    extend(pts, s + delta, p);
+    delta++;
+  }
+  return delta;
+}
+
+top::f_t top::frame(const p_t* pts, size_t s)
+{
+  if (!s) {
+    throw std::logic_error("bad size");
+  }
+  int minx = pts[0].x, maxx = minx;
+  int miny = pts[0].y, maxy = miny;
+  for (size_t i = 1; i < s; i++) {
+    minx = std::min(minx, pts[i].x);
+    maxx = std::max(maxx, pts[i].x);
+    miny = std::min(miny, pts[i].y);
+    maxy = std::max(maxy, pts[i].y);
+  }
+  p_t aa{minx, miny};
+  p_t bb{maxx, maxy};
+  return {aa, bb};
+}
+
+char * top::canvas(f_t fr, char fill) 
+{
+  char * cnv = new char[rows(fr) * cols(fr)];
+  for (size_t i = 0; i < rows(fr) * cols(fr); i++) {
+    cnv[i] = fill;
+  }
+  return cnv;
+}
+
+void top::paint(char* cnv, f_t fr, p_t p, char fill)
+{
+  int dx = p.x - fr.aa.x;
+  int dy = fr.bb.y - p.y;
+  cnv[dy * cols(fr) + dx] = fill;
+}
+
+void top::flush(std::ostream& os, const char* cnv, f_t fr)
+{
+  for (size_t i = 0; i < rows(fr); i++) {
+    for (size_t j = 0; j < cols(fr); j++) {
+      os << cnv[i * cols(fr) + j];
+    }
+    os << "\n";
+  }
 }
